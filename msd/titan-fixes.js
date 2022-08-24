445e866ff8637425b013966b06936f226213be6c
William Kurzius <bill@kurzius.net>
2022-08-24 08:14:56 -0400

    Blackboard lacks titles and competency

--- msd/titan-fixes.js
+++ msd/titan-fixes.js
@@ -3,3 +3,11 @@
 /*$(window).load(function() {
     $(".sw-channel-item").children().attr("href","#");
 });*/
+
+// Staff directory doesn't support titles.
+// Putting a title in first name breaks the initials avatar.
+// Can't embed code since Blackboard loads a separate page on search.
+// This is absurd.
+window.onload = function(){
+    document.querySelector('div[data-lastname="Maroun"]').innerHTML = "JM";
+};
