741417522ac3907d3f66fd4d52f93514c5cf5d1c
William Kurzius <bill@kurzius.net>
2022-08-24 08:42:29 -0400

    CSS fix instead of JS

--- msd/titan-fixes.js
+++ msd/titan-fixes.js
@@ -3,12 +3,3 @@
 /*$(window).load(function() {
     $(".sw-channel-item").children().attr("href","#");
 });*/
-
-// Staff directory doesn't support titles.
-// Putting a title in first name breaks the initials avatar.
-// Can't embed code since Blackboard loads a separate page on search.
-// This is absurd.
-// Oh, and it's onresize because they don't use onload.
-window.onresize = function(){
-    document.querySelector('div[data-lastname="Maroun"]').innerHTML = "JM";
-};
