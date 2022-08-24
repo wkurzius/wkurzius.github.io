86545afd0420869784aef279bba632efee0aeda4
William Kurzius <bill@kurzius.net>
2022-08-24 08:31:17 -0400

    onload to onresize, fml

--- msd/titan-fixes.js
+++ msd/titan-fixes.js
@@ -8,6 +8,7 @@
 // Putting a title in first name breaks the initials avatar.
 // Can't embed code since Blackboard loads a separate page on search.
 // This is absurd.
-window.onload = function(){
+// Oh, and it's onresize because they don't use onload.
+window.onresize = function(){
     document.querySelector('div[data-lastname="Maroun"]').innerHTML = "JM";
 };
